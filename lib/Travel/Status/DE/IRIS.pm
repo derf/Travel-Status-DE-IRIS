derf/Travel-Status-DE-IRIS

commit 2c41eecea9d7d5ae563e0390514336e95b7c0c0a

IRIS.pm: Errorhandling for stations without associated timetable

lib/Travel/Status/DE/IRIS.pm
@@ -49,6 +49,12 @@ sub new {
 
 	$self->{nodes}{station} = ( $xml_st->findnodes('//station') )[0];
 
+	if ( not $self->{node}->{station} ) {
+		$self->{errstr}
+		  = "The station '$opt{station}' has no associated timetable";
+		return $self;
+	}
+
 	my $dt_req = $self->{datetime}->clone;
 	for ( 1 .. 3 ) {
 		$self->get_timetable( $self->{nodes}{station}->getAttribute('eva'),

commit a7491a345f71b7805d2141424e505408e46e1297

IRIS.pm: Fix typo introduced in 2c41eecea9d7d5ae563e0390514336e95b7c0c0a

lib/Travel/Status/DE/IRIS.pm
@@ -49,7 +49,7 @@ sub new {
 
 	$self->{nodes}{station} = ( $xml_st->findnodes('//station') )[0];
 
-	if ( not $self->{node}->{station} ) {
+	if ( not $self->{nodes}{station} ) {
 		$self->{errstr}
 		  = "The station '$opt{station}' has no associated timetable";
 		return $self;