a2c046e98bc0cdcb102c4758e808a252dd3e7224
Daniel Friesel <derf@finalrewind.org>
2014-12-29 16:41:37 +0100

    Schnee und Eis: verified

--- lib/Travel/Status/DE/IRIS/Result.pm
+++ lib/Travel/Status/DE/IRIS/Result.pm
@@ -549,7 +549,7 @@ sub translate_msg {
 		55 => 'Technische Störung an einem anderen Zug',        # ?
 		57 => 'Zusätzlicher Halt',
 		58 => 'Umleitung',                                       # ?
-		59 => 'Schnee und Eis', # ?
+		59 => 'Schnee und Eis',
 		61 => 'Türstörung',
 		62 => 'Behobene technische Störung am Zug',
 		63 => 'Technische Untersuchung am Zug',
