b409b51f30427af3427b0e0e5fcb76f202b7d1b7
Daniel Friesel <derf@finalrewind.org>
2016-09-27 20:52:29 +0200

    get_station: Support EVA / UIC code

--- lib/Travel/Status/DE/IRIS/Stations.pm
+++ lib/Travel/Status/DE/IRIS/Stations.pm
@@ -8488,6 +8488,12 @@ sub get_station {
 		return ($ds100_match);
 	}
 
+	my $eva_match = firstval { defined $_->[2] and $name eq $_->[2] } @stations;
+
+	if ($eva_match) {
+		return ($eva_match);
+	}
+
 	return get_station_by_name($name);
 }
 
