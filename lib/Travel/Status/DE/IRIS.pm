cc27bc6667f26c111cfb8e7246908de128c2fc6a
Daniel Friesel <derf@finalrewind.org>
2019-06-23 07:59:23 +0200

    release v1.28

--- lib/Travel/Status/DE/IRIS.pm
+++ lib/Travel/Status/DE/IRIS.pm
@@ -6,7 +6,7 @@ use 5.014;
 
 no if $] >= 5.018, warnings => 'experimental::smartmatch';
 
-our $VERSION = '1.27';
+our $VERSION = '1.28';
 
 use Carp qw(confess cluck);
 use DateTime;
@@ -613,7 +613,7 @@ Travel::Status::DE::IRIS - Interface to IRIS based web departure monitors.
 
 =head1 VERSION
 
-version 1.27
+version 1.28
 
 =head1 DESCRIPTION
 
--- lib/Travel/Status/DE/IRIS/Result.pm
+++ lib/Travel/Status/DE/IRIS/Result.pm
@@ -15,7 +15,7 @@ use List::Compare;
 use List::MoreUtils qw(none uniq firstval);
 use Scalar::Util qw(weaken);
 
-our $VERSION = '1.27';
+our $VERSION = '1.28';
 
 my %translation = (
 	2  => 'Polizeiliche Ermittlung',
@@ -813,7 +813,7 @@ arrival/departure received by Travel::Status::DE::IRIS
 
 =head1 VERSION
 
-version 1.27
+version 1.28
 
 =head1 DESCRIPTION
 
