444cd6f65b2e10af3e9d325c2905cc559059bc97
Daniel Friesel <daniel.friesel@uos.de>
2019-07-17 17:23:42 +0200

    Release v1.30

--- lib/Travel/Status/DE/IRIS.pm
+++ lib/Travel/Status/DE/IRIS.pm
@@ -6,7 +6,7 @@ use 5.014;
 
 no if $] >= 5.018, warnings => 'experimental::smartmatch';
 
-our $VERSION = '1.29';
+our $VERSION = '1.30';
 
 use Carp qw(confess cluck);
 use DateTime;
@@ -614,7 +614,7 @@ Travel::Status::DE::IRIS - Interface to IRIS based web departure monitors.
 
 =head1 VERSION
 
-version 1.29
+version 1.30
 
 =head1 DESCRIPTION
 
--- lib/Travel/Status/DE/IRIS/Result.pm
+++ lib/Travel/Status/DE/IRIS/Result.pm
@@ -15,7 +15,7 @@ use List::Compare;
 use List::MoreUtils qw(none uniq firstval);
 use Scalar::Util qw(weaken);
 
-our $VERSION = '1.29';
+our $VERSION = '1.30';
 
 my %translation = (
 	2  => 'Polizeiliche Ermittlung',
@@ -816,7 +816,7 @@ arrival/departure received by Travel::Status::DE::IRIS
 
 =head1 VERSION
 
-version 1.29
+version 1.30
 
 =head1 DESCRIPTION
 
