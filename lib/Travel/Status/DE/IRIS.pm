7dfdca523325bec7c92e62d312efae4b2be91d06
Daniel Friesel <derf@finalrewind.org>
2019-11-17 10:27:53 +0100

    Release v1.35

--- lib/Travel/Status/DE/IRIS.pm
+++ lib/Travel/Status/DE/IRIS.pm
@@ -6,7 +6,7 @@ use 5.014;
 
 no if $] >= 5.018, warnings => 'experimental::smartmatch';
 
-our $VERSION = '1.34';
+our $VERSION = '1.35';
 
 use Carp qw(confess cluck);
 use DateTime;
@@ -624,7 +624,7 @@ Travel::Status::DE::IRIS - Interface to IRIS based web departure monitors.
 
 =head1 VERSION
 
-version 1.34
+version 1.35
 
 =head1 DESCRIPTION
 
