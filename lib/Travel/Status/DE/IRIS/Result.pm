7b2c3669c496436b650d8a844e2625d1408d038c
Daniel Friesel <derf@finalrewind.org>
2020-10-03 14:36:43 +0200

    Release v1.49

--- lib/Travel/Status/DE/IRIS/Result.pm
+++ lib/Travel/Status/DE/IRIS/Result.pm
@@ -15,7 +15,7 @@ use List::Compare;
 use List::MoreUtils qw(none uniq firstval);
 use Scalar::Util qw(weaken);
 
-our $VERSION = '1.48';
+our $VERSION = '1.49';
 
 my %translation = (
 	2  => 'Polizeiliche Ermittlung',
@@ -841,7 +841,7 @@ arrival/departure received by Travel::Status::DE::IRIS
 
 =head1 VERSION
 
-version 1.48
+version 1.49
 
 =head1 DESCRIPTION
 
