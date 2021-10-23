f4d2b548ce39521d098c5c766283bb27fc81456b
Daniel Friesel <derf@finalrewind.org>
2021-10-23 09:57:02 +0200

    remove legacy acronyms.sh / .pl scripts

--- scripts/acronyms.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -s http://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2017_09.csv \
-| perl scripts/acronyms.pl \
-> lib/Travel/Status/DE/IRIS/Stations.pm
