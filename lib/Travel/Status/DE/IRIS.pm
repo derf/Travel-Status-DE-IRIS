f61e0453ccf37c3fff6abf6ee1ed23a8ac105723
Daniel Friesel <derf@finalrewind.org>
2022-08-02 11:00:45 +0200

    Release v1.69 (oops)

--- lib/Travel/Status/DE/IRIS.pm
+++ lib/Travel/Status/DE/IRIS.pm
@@ -6,7 +6,7 @@ use 5.014;
 
 no if $] >= 5.018, warnings => 'experimental::smartmatch';
 
-our $VERSION = '1.68';
+our $VERSION = '1.69';
 
 use Carp qw(confess cluck);
 use DateTime;
@@ -964,7 +964,7 @@ Non-blocking variant (EXPERIMENTAL):
 
 =head1 VERSION
 
-version 1.68
+version 1.69
 
 =head1 DESCRIPTION
 
--- lib/Travel/Status/DE/IRIS/Result.pm
+++ lib/Travel/Status/DE/IRIS/Result.pm
@@ -15,7 +15,7 @@ use List::Compare;
 use List::MoreUtils qw(none uniq lastval);
 use Scalar::Util qw(weaken);
 
-our $VERSION = '1.68';
+our $VERSION = '1.69';
 
 my %translation = (
 	1 => 'Nähere Informationen in Kürze',
@@ -876,7 +876,7 @@ arrival/departure received by Travel::Status::DE::IRIS
 
 =head1 VERSION
 
-version 1.68
+version 1.69
 
 =head1 DESCRIPTION
 
