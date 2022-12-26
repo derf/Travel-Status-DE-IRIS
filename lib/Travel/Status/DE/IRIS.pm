528c5738f20f4466c3bb69035a5845c691b53b0c
Daniel Friesel <derf@finalrewind.org>
2022-12-26 09:51:24 +0100

    Release v1.79

--- lib/Travel/Status/DE/IRIS.pm
+++ lib/Travel/Status/DE/IRIS.pm
@@ -6,7 +6,7 @@ use 5.014;
 
 no if $] >= 5.018, warnings => 'experimental::smartmatch';
 
-our $VERSION = '1.78';
+our $VERSION = '1.79';
 
 use Carp qw(confess cluck);
 use DateTime;
@@ -987,7 +987,7 @@ Non-blocking variant (EXPERIMENTAL):
 
 =head1 VERSION
 
-version 1.78
+version 1.79
 
 =head1 DESCRIPTION
 
