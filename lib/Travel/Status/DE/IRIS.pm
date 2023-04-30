06702f5bdbcd5bbc47e74b0d8c38cac2f29e6147
Derf Null <derf@finalrewind.org>
2023-04-30 07:48:00 +0200

    Release v1.84

--- lib/Travel/Status/DE/IRIS.pm
+++ lib/Travel/Status/DE/IRIS.pm
@@ -6,7 +6,7 @@ use 5.014;
 
 no if $] >= 5.018, warnings => 'experimental::smartmatch';
 
-our $VERSION = '1.83';
+our $VERSION = '1.84';
 
 use Carp qw(confess cluck);
 use DateTime;
@@ -987,7 +987,7 @@ Non-blocking variant (EXPERIMENTAL):
 
 =head1 VERSION
 
-version 1.83
+version 1.84
 
 =head1 DESCRIPTION
 
