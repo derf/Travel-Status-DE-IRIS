62fd3e6f7a1269129ab12d3343561875e52f44b4
Derf Null <derf@finalrewind.org>
2023-05-29 14:34:17 +0200

    Release v1.85

--- lib/Travel/Status/DE/IRIS/Result.pm
+++ lib/Travel/Status/DE/IRIS/Result.pm
@@ -15,7 +15,7 @@ use List::Compare;
 use List::MoreUtils qw(none uniq lastval);
 use Scalar::Util    qw(weaken);
 
-our $VERSION = '1.84';
+our $VERSION = '1.85';
 
 Travel::Status::DE::IRIS::Result->mk_ro_accessors(
 	qw(arrival arrival_delay arrival_has_realtime arrival_is_additional arrival_is_cancelled arrival_hidden
@@ -920,7 +920,7 @@ arrival/departure received by Travel::Status::DE::IRIS
 
 =head1 VERSION
 
-version 1.84
+version 1.85
 
 =head1 DESCRIPTION
 
