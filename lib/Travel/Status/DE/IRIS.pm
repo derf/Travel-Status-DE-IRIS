178c7cf3112784e720e3c08b772d1a7ac79181f5
Birte Kristina Friesel <derf@finalrewind.org>
2023-07-09 16:30:13 +0200

    Release v1.87

--- lib/Travel/Status/DE/IRIS.pm
+++ lib/Travel/Status/DE/IRIS.pm
@@ -6,7 +6,7 @@ use 5.014;
 
 no if $] >= 5.018, warnings => 'experimental::smartmatch';
 
-our $VERSION = '1.86';
+our $VERSION = '1.87';
 
 use Carp qw(confess cluck);
 use DateTime;
@@ -987,7 +987,7 @@ Non-blocking variant (EXPERIMENTAL):
 
 =head1 VERSION
 
-version 1.86
+version 1.87
 
 =head1 DESCRIPTION
 
